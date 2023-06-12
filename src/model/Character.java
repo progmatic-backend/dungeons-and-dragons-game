b463948ccfbf18a1a42fed841d03eae5ddb7b78d
Liliána Bohus <boli96@gmail.com>
2023-06-12 17:13:41 +0200

    fix minor typos

--- src/model/Character.java
+++ src/model/Character.java
@@ -6,12 +6,12 @@ public class Character {
     // Írj egy egy paraméteres kosntruktort ami beállítja a nevet és a health 100-ra, a levelt pedig 1-re állítja!
 
     public void attack(Monster monster) {
-        // Írd ki: [játékos neve] attacks [szötny neve]!
+        // Írd ki: [játékos neve] attacks [szörny neve]!
 
         // Készíts egy damage változót, legyen az értéke a level 10-szerese!
         // Vond le ezt az értéket a szörny health-jéből!
 
-        // Írd ki: Dealt [damage változó értéke] damage to [játékos neve]!
+        // Írd ki: Dealt [damage változó értéke] damage to [szörny neve]!
 
         // Ha a szörny élete 0 vagy kisebb, írd ki: [szörny neve] has been defeated!
     }
